3d8a648cbb2c195c31ce428cb1289f924022b183
Murad <49407545+Murad14@users.noreply.github.com>
2023-01-19 22:01:55 +0600

    first phase done

--- app.js
+++ app.js
@@ -6,9 +6,18 @@ const axios = require('axios'); //axios
 console.log(axios.isCancel('something'));
 
 app.use(express.static("public"));
+app.use(express.urlencoded()); //BODY-PARSER
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/signup.html");
+})
+
+app.post("/", function(req,res){
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var email = req.body.email;
+
+    console.log(firstName,lastName,email);
 })
 
 // app.get('/', (req, res) => {
