9f0c4218e89aba240b7fb593919b8909b4cd59e3
Murad <49407545+Murad14@users.noreply.github.com>
2023-01-19 21:45:33 +0600

    modified

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ const axios = require('axios'); //axios
 
 console.log(axios.isCancel('something'));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
 })
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
